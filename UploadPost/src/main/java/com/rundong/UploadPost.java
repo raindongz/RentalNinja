4ce9753dad339e67ac985746437fd5e24811a77f
rundongzhong <ryandzhong@gmail.com>
2024-09-07 18:46:13 -0700

    test if github ci-cd works

--- UploadPost/src/main/java/com/rundong/UploadPost.java
+++ UploadPost/src/main/java/com/rundong/UploadPost.java
@@ -5,11 +5,11 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 /**
- * Rundong Zhong
+ * Hello world!
  *
  */
-public class UploadPicturesHandler implements RequestHandler<String, String> {
-
+public class UploadPost implements RequestHandler<String, String>
+{
     @Override
     public String handleRequest(String input, Context context) {
         LambdaLogger logger = context.getLogger();
