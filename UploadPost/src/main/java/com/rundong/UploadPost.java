4c026ff7c838966b482ae62e7188c5db84dfce31
rundongzhong <ryandzhong@gmail.com>
2024-09-13 17:40:39 -0700

    added error handler for uploadpost function

--- UploadPost/src/main/java/com/rundong/UploadPost.java
+++ UploadPost/src/main/java/com/rundong/UploadPost.java
@@ -1,6 +1,5 @@
 package com.rundong;
 
-import com.amazonaws.Response;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
@@ -12,6 +11,7 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import com.amazonaws.services.lambda.runtime.logging.LogLevel;
 import com.amazonaws.util.StringUtils;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -27,18 +27,13 @@ public class UploadPost implements RequestHandler<APIGatewayProxyRequestEvent, A
     private static final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
     private static final DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(client);
 
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
     @Override
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent event, final Context context) {
-        // set up response header
-        Map<String, String> responseHeaders = new HashMap<>();
-        responseHeaders.put("Content-Type", "application/json");
-        responseHeaders.put("X-Custom-Header", "application/json");
-        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent().withHeaders(responseHeaders);
         // initialize logger
         LambdaLogger logger = context.getLogger();
         logger.log("Function" + context.getFunctionName() + "  is called", LogLevel.INFO);
 
-        Gson gson = new Gson();
         Post post = gson.fromJson(event.getBody(), Post.class);
         post.setCreateTime(new Date());
         post.setUpdateTime(new Date());
@@ -48,16 +43,33 @@ public class UploadPost implements RequestHandler<APIGatewayProxyRequestEvent, A
             dynamoDBMapper.save(post);
         }catch (Exception e){
             logger.log("save post to dynamodb error:  " + e, LogLevel.ERROR);
-            // todo this need to be modified with error handler
-            return response
-                    .withStatusCode(500)
-                    .withBody("internal server error!");
+            return returnApiResponse(500, "db error", "db error", "500", logger);
         }
         String json = gson.toJson("success!");
 
-        return response
-                .withStatusCode(200)
-                .withBody(json);
+        return returnApiResponse(200, json, null, null, logger);
+    }
+    public APIGatewayProxyResponseEvent returnApiResponse(int statusCode, String responseBody,
+                                                          String errorMessage, String errorCode, LambdaLogger logger){
+        final Error error = new Error();
+        if(!StringUtils.isNullOrEmpty(errorCode)){
+            error.setErrorCode(errorCode);
+            error.setErrorMessage(errorMessage);
+        }
+
+        // Prepare response header
+        Map<String, String> responseHeaders = new HashMap<>();
+        responseHeaders.put("Content-Type", "application/json");
+        responseHeaders.put("X-Custom-Header", "application/json");
+
+        APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent()
+                .withHeaders(responseHeaders)
+                .withStatusCode(statusCode)
+                .withBody(gson.toJson(new Response<String>(statusCode, responseBody, error)));
+        logger.log("\n" + responseEvent.toString(), LogLevel.INFO);
+
+        return responseEvent;
+
     }
 
 }
