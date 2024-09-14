4cff3abd77b197b6f5302519b56512f915d73096
rundongzhong <ryandzhong@gmail.com>
2024-09-13 17:56:20 -0700

    added error handler for getpostdetail function

--- GetSpecificPostDetail/src/main/java/com/rundong/GetSpecificPostDetail.java
+++ GetSpecificPostDetail/src/main/java/com/rundong/GetSpecificPostDetail.java
@@ -9,7 +9,9 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.amazonaws.services.lambda.runtime.logging.LogLevel;
+import com.amazonaws.util.StringUtils;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -23,6 +25,7 @@ public class GetSpecificPostDetail implements RequestHandler<APIGatewayProxyRequ
 {
     private static final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
     private static final DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(client);
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
     @Override
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent event, final Context context) {
         // set up response header
@@ -34,7 +37,6 @@ public class GetSpecificPostDetail implements RequestHandler<APIGatewayProxyRequ
         LambdaLogger logger = context.getLogger();
         logger.log("Function" + context.getFunctionName() + "  is called", LogLevel.INFO);
 
-        Gson gson = new Gson();
         Request request = gson.fromJson(event.getBody(), Request.class);
         String postId = request.postId();
         logger.log("this is post ID: " + postId, LogLevel.INFO);
@@ -44,19 +46,34 @@ public class GetSpecificPostDetail implements RequestHandler<APIGatewayProxyRequ
         try {
              post = dynamoDBMapper.load(Post.class, postId);
         }catch (Exception e){
-            //todo: throw new exception
             logger.log("load post with postId error "+e, LogLevel.ERROR);
-            return new APIGatewayProxyResponseEvent()
-                    .withHeaders(responseHeaders)
-                    .withBody("internal server error!!")
-                    .withStatusCode(500);
+            return returnApiResponse(500, "db error", "db error, please try again", "500", logger);
         }
         // convert to json object then return to client
         String json = gson.toJson(post);
 
-        return new APIGatewayProxyResponseEvent()
+        return returnApiResponse(200, json, null, null, logger);
+    }
+
+    public APIGatewayProxyResponseEvent returnApiResponse(int statusCode, String responseBody,
+                                                          String errorMessage, String errorCode, LambdaLogger logger){
+        final Error error = new Error();
+        if(!StringUtils.isNullOrEmpty(errorCode)){
+            error.setErrorCode(errorCode);
+            error.setErrorMessage(errorMessage);
+        }
+
+        // Prepare response header
+        Map<String, String> responseHeaders = new HashMap<>();
+        responseHeaders.put("Content-Type", "application/json");
+
+        APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent()
                 .withHeaders(responseHeaders)
-                .withBody(json)
-                .withStatusCode(200);
+                .withStatusCode(statusCode)
+                .withBody(gson.toJson(new Response<String>(statusCode, responseBody, error)));
+        logger.log("\n" + responseEvent.toString(), LogLevel.INFO);
+
+        return responseEvent;
+
     }
 }
